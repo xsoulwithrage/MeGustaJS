c6dd2fb6d7d053df89527debea22b5a446c20e98
Ed <101188188+xsoulwithrage@users.noreply.github.com>
2022-10-20 12:09:22 -0300

    Add files via upload

---
+++ Me gusta “me gusta”/script/script1.js
@@ -0,0 +1,9 @@
+console.log("probando si funciona");
+
+var NumerodeLikes = 3;
+var Contadorlikes = document.querySelector("#numero");
+
+function corazones1(){
+NumerodeLikes++;
+Contadorlikes.innerText = NumerodeLikes
+}
---
+++ Me gusta “me gusta”/script/script2.js
@@ -0,0 +1,23 @@
+var NumerodeLikes = 9;
+var Contadorlikes = document.querySelector("#numero");
+
+function corazones1(){
+NumerodeLikes++;
+Contadorlikes.innerText = NumerodeLikes
+}
+
+var NumerodeLikes1 = 12;
+var Contadorlikes1 = document.querySelector("#numero2");
+
+function corazones2(){
+NumerodeLikes1++;
+Contadorlikes1.innerText = NumerodeLikes1
+}
+
+var NumerodeLikes2 = 12;
+var Contadorlikes2 = document.querySelector("#numero1");
+
+function corazones3(){
+NumerodeLikes1++;
+Contadorlikes2.innerText = NumerodeLikes2
+}
